d68ab28e2f60a43d773e17b5bbf4a993f7825319
naimishsrivastava <sriv.naimish@gmail.com>
2022-03-04 01:23:44 +0530

    Updates

---
+++ precache-manifest.6efbcc0d0fbd577bafbb57f852ce870d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072b0df6b19566bba99896a837dce9e6",
+    "url": "/tele-chat/index.html"
+  },
+  {
+    "revision": "32ca8b02e721c39cd87e",
+    "url": "/tele-chat/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "1aaeb921ee194b5a53cd",
+    "url": "/tele-chat/static/css/4.cdfe642f.chunk.css"
+  },
+  {
+    "revision": "65b04d03186b826e30b1",
+    "url": "/tele-chat/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "1bdfcdabaaef04942d90",
+    "url": "/tele-chat/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "3e3cbd14c987e9af540e",
+    "url": "/tele-chat/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "32ca8b02e721c39cd87e",
+    "url": "/tele-chat/static/js/0.dc07a68c.chunk.js"
+  },
+  {
+    "revision": "f77df77b0064a06a0ced",
+    "url": "/tele-chat/static/js/3.c014426f.chunk.js"
+  },
+  {
+    "revision": "1aaeb921ee194b5a53cd",
+    "url": "/tele-chat/static/js/4.432f0887.chunk.js"
+  },
+  {
+    "revision": "65b04d03186b826e30b1",
+    "url": "/tele-chat/static/js/5.21c03230.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/tele-chat/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "1bdfcdabaaef04942d90",
+    "url": "/tele-chat/static/js/7.e805f045.chunk.js"
+  },
+  {
+    "revision": "3e3cbd14c987e9af540e",
+    "url": "/tele-chat/static/js/main.cc53703a.chunk.js"
+  },
+  {
+    "revision": "b6aad81949695b0ef0e8",
+    "url": "/tele-chat/static/js/runtime~main.3def3f66.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/tele-chat/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/tele-chat/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/tele-chat/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/tele-chat/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
